e52d0be0547de261e9d8a2488d50288bdaae763f
Matt <mattdenni@gmail.com>
2020-05-12 07:20:28 +0200

    Removed .ipynb conversion script

--- forecast_script.R
+++
@@ -1,6 +0,0 @@
-#Convert .ipynb to RMarkdown
-library(xfun)
-
-input_file <- "/Users/mattdenni/Documents/Data Science/Projects for CV/Functions for Food Price Forecasts/food-price-forecasting.ipynb"
-
-rmd_file <- rmarkdown:::convert_ipynb(input_file, output = with_ext(input_file, "Rmd"))
